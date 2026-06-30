9db96d73ef93a72fc5660f6541d02692ee76b0f9
Aissam <aissamdjidejlli8.com>
2026-06-30 20:10:12 +0100

    Adding Button and CheckBox

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.btnLogin = new System.Windows.Forms.Button();
             this.lbLogIn = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btnLogin
@@ -37,9 +39,10 @@
             this.btnLogin.BackColor = System.Drawing.Color.Salmon;
             this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
-            this.btnLogin.Location = new System.Drawing.Point(276, 135);
+            this.btnLogin.Location = new System.Drawing.Point(414, 208);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(220, 86);
+            this.btnLogin.Size = new System.Drawing.Size(330, 132);
             this.btnLogin.TabIndex = 0;
             this.btnLogin.Text = "Login";
             this.btnLogin.UseVisualStyleBackColor = false;
@@ -49,18 +52,43 @@
             // 
             this.lbLogIn.AutoSize = true;
             this.lbLogIn.Font = new System.Drawing.Font("Segoe Print", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbLogIn.Location = new System.Drawing.Point(264, 235);
+            this.lbLogIn.Location = new System.Drawing.Point(396, 362);
+            this.lbLogIn.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbLogIn.Name = "lbLogIn";
-            this.lbLogIn.Size = new System.Drawing.Size(0, 70);
+            this.lbLogIn.Size = new System.Drawing.Size(0, 105);
             this.lbLogIn.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Salmon;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.button1.Location = new System.Drawing.Point(800, 208);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(227, 79);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Login 2";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(800, 314);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(227, 26);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "Login 2";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.lbLogIn);
             this.Controls.Add(this.btnLogin);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -72,6 +100,8 @@
 
         private System.Windows.Forms.Button btnLogin;
         private System.Windows.Forms.Label lbLogIn;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
